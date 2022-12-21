7a308695a105e1914d11d56ff37574cc74300859
fashionbrot <fashionbrot@163.com>
2022-12-21 23:33:41 +0800

    基础功能开发完成

--- doc-test/src/main/java/com/github/fashionbrot/controller/DownloadController.java
+++ doc-test/src/main/java/com/github/fashionbrot/controller/DownloadController.java
@@ -53,12 +53,55 @@ public class DownloadController {
             // 告知浏览器文件的大小
             response.addHeader("Content-Length", "" + file.length());
             OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-            response.setContentType("application/octet-stream");
+            response.setContentType(getContentType(filename));
             outputStream.write(buffer);
             outputStream.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+
+    public static  String getContentType(String fileName){
+        //文件名后缀
+        String fileExtension = fileName.substring(fileName.lastIndexOf("."));
+        if(".bmp".equalsIgnoreCase(fileExtension)) {
+            return "image/bmp";
+        }
+        if(".gif".equalsIgnoreCase(fileExtension)) {
+            return "image/gif";
+        }
+        if(".jpeg".equalsIgnoreCase(fileExtension) || ".jpg".equalsIgnoreCase(fileExtension)  || ".png".equalsIgnoreCase(fileExtension) ){
+            return "image/jpeg";
+        }
+        if(".html".equalsIgnoreCase(fileExtension)){
+            return "text/html";
+        }
+        if(".txt".equalsIgnoreCase(fileExtension)){
+            return "text/plain";
+        }
+        if(".vsd".equalsIgnoreCase(fileExtension)){
+            return "application/vnd.visio";
+        }
+        if(".ppt".equalsIgnoreCase(fileExtension) || "pptx".equalsIgnoreCase(fileExtension)) {
+            return "application/vnd.ms-powerpoint";
+        }
+        if(".doc".equalsIgnoreCase(fileExtension) || "docx".equalsIgnoreCase(fileExtension)) {
+            return "application/msword";
+        }
+        if(".xml".equalsIgnoreCase(fileExtension)) {
+            return "text/xml";
+        }
+        if(".doc".equalsIgnoreCase(fileExtension)) {
+            return "application/msword";
+        }
+        if(".docx".equalsIgnoreCase(fileExtension)) {
+            return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+        }
+        if(".pdf".equalsIgnoreCase(fileExtension)) {
+            return "application/pdf";
+        }
+        return "application/octet-stream";
     }
 
 
