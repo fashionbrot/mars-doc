9e852476c07353a79b9d636c59a9d12aac3cba0c
fashionbrot <fashionbrot@163.com>
2023-01-03 17:49:04 +0800

    修复显示问题

--- doc-test/src/main/java/com/github/fashionbrot/controller/ResponseController.java
+++ doc-test/src/main/java/com/github/fashionbrot/controller/ResponseController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * @author fashionbrot
  */
+
 @Api(value = "responseTest接口",priority = 97)
 @Controller
 @RequestMapping("/response")
